dfbcfbdba488b4f32d4789fcb3d0e554b37985a3
arjunrai-1T <arjunrai.email@gmail.com>
2024-11-26 01:54:32 +0530

    initial commit

---
+++ src/test/java/com/xapp/member/XappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xapp.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
